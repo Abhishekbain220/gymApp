3d96dff63ca4c8aac08af6715387aaf545ab5e6c
abhishekbain220 <you@example.co>
2025-05-06 18:14:46 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(morgan("tiny"))
 app.use(cors({
-    origin: true,
+    origin: "https://gymapp-wcgp.onrender.com",
     credentials: true
 }))
 
--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance=axios.create({
-    baseURL:"http://localhost:3000/",
+    baseURL:"https://gymapp-wcgp.onrender.com",
     withCredentials:true
 })
 
