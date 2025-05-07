8f69dc9bcd6353751d84d8a5109206c2dae49e2f
abhishekbain220 <you@example.co>
2025-05-07 17:47:06 +0530

    16

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+    <div className=" absolute z-70 flex items-center justify-center h-screen bg-gray-900 text-white">
             <div className="flex flex-col items-center">
                 <motion.div
                     className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
